8736c566dde08f583a07ae13495a81f7e8991b71
ManeeshaMaduranga <MadurangaBandara1006@gmail.com>
2023-08-02 14:30:52 +0530

    Add Signin And signup page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -15,12 +15,18 @@ import HomePage from './pages/HomePage';
 import PropertyPage from './pages/PropertyPage.jsx';
 import AboutPage from './pages/AboutPage.jsx';
 
+//Auth Pages
+import SigninPage from './pages/Auth/SigninPage.jsx';
+import SignupPage from './pages/Auth/SignupPage.jsx';
+
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App />}>
       <Route index element={<HomePage />} />
       <Route path='/about' element={<AboutPage />} />
       <Route path='/property' element={<PropertyPage />} />
+      <Route path='/signin' element={<SigninPage />} />
+      <Route path='/signup' element={<SignupPage />} />
     </Route>
   )
 );
---
+++ frontend/src/pages/Auth/SignupPage.jsx
@@ -0,0 +1,84 @@
+import { Link } from 'react-router-dom';
+
+function SignupPage() {
+  return (
+    <div className='mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8'>
+      <div className='mx-auto max-w-lg text-center'>
+        <h1 className='text-2xl font-bold sm:text-3xl'>Get started today!</h1>
+      </div>
+      <form action='' className='mx-auto mb-0 mt-8 max-w-md space-y-4'>
+        <div>
+          <label htmlFor='email' className='sr-only'>
+            Email
+          </label>
+
+          <div className='relative'>
+            <input
+              type='email'
+              className='w-full rounded-lg border-gray-200 border-2 p-4 pe-12 text-sm shadow-sm'
+              placeholder='Enter email'
+            />
+          </div>
+        </div>
+        <div>
+          <label htmlFor='email' className='sr-only'>
+            User Name
+          </label>
+
+          <div className='relative'>
+            <input
+              type='email'
+              className='w-full rounded-lg border-gray-200 border-2 p-4 pe-12 text-sm shadow-sm'
+              placeholder='Enter user Name'
+            />
+          </div>
+        </div>
+
+        <div>
+          <label htmlFor='password' className='sr-only'>
+            Password
+          </label>
+
+          <div className='relative'>
+            <input
+              type='password'
+              className='w-full rounded-lg border-gray-200 border-2 p-4 pe-12 text-sm shadow-sm'
+              placeholder='Password'
+            />
+          </div>
+        </div>
+        <div>
+          <label htmlFor='password' className='sr-only'>
+            Password Confirmation
+          </label>
+
+          <div className='relative'>
+            <input
+              type='password'
+              className='w-full rounded-lg border-gray-200 border-2 p-4 pe-12 text-sm shadow-sm'
+              placeholder='Password Confirmation'
+            />
+          </div>
+        </div>
+
+        <div className='flex items-center justify-between'>
+          <p className='text-sm text-gray-500'>
+            Already have an account?
+            <Link className='underline' to='/signin'>
+              Sign in
+            </Link>
+          </p>
+
+          <button
+            type='submit'
+            className='inline-block rounded-lg bg-green-400 px-5 py-3 text-sm font-medium text-white'
+          >
+            Sign up
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default SignupPage;
